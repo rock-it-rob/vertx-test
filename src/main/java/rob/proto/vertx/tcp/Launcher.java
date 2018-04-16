695a099704f16f348c044da13180adbe00638e81
Rob Benton <rob.benton7@gmail.com>
2018-04-16 14:17:15 -0500

    fresh batch of code

---
+++ src/main/java/rob/proto/vertx/tcp/Launcher.java
@@ -0,0 +1,21 @@
+package rob.proto.vertx.tcp;
+
+import io.vertx.core.Vertx;
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+public class Launcher
+{
+    private static final String SPRING_XML = "classpath:server-spring.xml";
+
+    public static void main(String[] args)
+    {
+        try (
+                GenericXmlApplicationContext applicationContext = new GenericXmlApplicationContext(SPRING_XML)
+        )
+        {
+            Vertx vertx = Vertx.vertx();
+            ServerVerticle serverVerticle = applicationContext.getBean(ServerVerticle.class);
+            vertx.deployVerticle(serverVerticle);
+        }
+    }
+}
---
+++ src/main/java/rob/proto/vertx/tcp/ServerVerticle.java
@@ -0,0 +1,19 @@
+package rob.proto.vertx.tcp;
+
+import io.vertx.core.AbstractVerticle;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * ServerVerticle runs a tcp server
+ * @author Rob Benton
+ */
+public class ServerVerticle extends AbstractVerticle
+{
+    private static final Logger log = LoggerFactory.getLogger(ServerVerticle.class);
+
+    public void start()
+    {
+        log.info("Starting server");
+    }
+}
