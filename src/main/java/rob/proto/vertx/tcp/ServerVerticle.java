cdbcf3b24fc6bc91bbea184eb12a468f30c8d606
Rob Benton <rob.benton7@gmail.com>
2018-04-16 14:19:27 -0500

    Added logging

--- src/main/java/rob/proto/vertx/tcp/ServerVerticle.java
+++ src/main/java/rob/proto/vertx/tcp/ServerVerticle.java
@@ -12,8 +12,15 @@ public class ServerVerticle extends AbstractVerticle
 {
     private static final Logger log = LoggerFactory.getLogger(ServerVerticle.class);
 
+    @Override
     public void start()
     {
         log.info("Starting server");
+    }
+
+    @Override
+    public void stop()
+    {
+        log.info("Stopping server");
     }
 }
